fd37813f8af8d2934f86355f1ae33621612c716e
Ventas <ventas@elixirgym.com>
2026-08-07 21:58:05 -0400

    style: swap zona de boxeo and entrenamiento funcional in gallery

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -10,8 +10,8 @@ import { CommonModule } from '@angular/common';
 export class Gallery {
   images = [
     { src: 'assets/images/area-de-musculacion.jpg', alt: 'Área de Musculación', span: 'col-span-1 row-span-1 md:row-span-1' },
-    { src: 'assets/images/entrenamiento-funcional.jpg', alt: 'Entrenamiento Funcional', span: 'col-span-1 row-span-1 md:row-span-2' },
-    { src: 'assets/images/zona-de-boxeo.jpg', alt: 'Zona de Boxeo', span: 'col-span-1 row-span-1 md:row-span-1' },
+    { src: 'assets/images/zona-de-boxeo.jpg', alt: 'Zona de Boxeo', span: 'col-span-1 row-span-1 md:row-span-2' },
+    { src: 'assets/images/entrenamiento-funcional.jpg', alt: 'Entrenamiento Funcional', span: 'col-span-1 row-span-1 md:row-span-1' },
     { src: 'assets/images/area-de-cardio.jpg', alt: 'Área de Cardio', span: 'col-span-1 row-span-1 md:row-span-1' },
     { src: 'assets/images/imagen-personas-entrenando.jpg', alt: 'Entrenamiento Libre', span: 'col-span-1 row-span-1 md:row-span-1' }
   ];
