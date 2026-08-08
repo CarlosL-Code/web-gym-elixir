ce40e3d786ec8cc95b2c74fe30dde4d41f1aa9a4
Ventas <ventas@elixirgym.com>
2026-08-07 21:49:01 -0400

    style: remove 3 images and restore original gallery layout

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -9,13 +9,10 @@ import { CommonModule } from '@angular/common';
 })
 export class Gallery {
   images = [
-    { src: 'assets/images/entrenamiento-funcional.jpg', alt: 'Entrenamiento Funcional', span: 'md:col-span-2 md:row-span-2' },
-    { src: 'assets/images/area-de-musculacion.jpg', alt: 'Área de Musculación', span: 'col-span-1 row-span-1' },
-    { src: 'assets/images/zona-de-boxeo.jpg', alt: 'Zona de Boxeo', span: 'col-span-1 row-span-1' },
+    { src: 'assets/images/area-de-musculacion.jpg', alt: 'Área de Musculación', span: 'md:col-span-2 md:row-span-2' },
     { src: 'assets/images/area-de-cardio.jpg', alt: 'Área de Cardio', span: 'col-span-1 row-span-1' },
-    { src: 'assets/images/imagen-personas-entrenando.jpg', alt: 'Entrenamiento Libre', span: 'col-span-1 row-span-1' },
-    { src: 'assets/images/discos-profesionales.jpg', alt: 'Discos y Pesas', span: 'md:col-span-2 md:row-span-1' },
-    { src: 'assets/images/maquinas-principales.jpg', alt: 'Máquinas Principales', span: 'md:col-span-2 md:row-span-1' },
-    { src: 'assets/images/sacos-de-boxeo.jpg', alt: 'Sacos de Boxeo', span: 'md:col-span-4 md:row-span-2 lg:md:row-span-1' }
+    { src: 'assets/images/entrenamiento-funcional.jpg', alt: 'Entrenamiento Funcional', span: 'col-span-1 row-span-1' },
+    { src: 'assets/images/zona-de-boxeo.jpg', alt: 'Zona de Boxeo', span: 'md:col-span-2 row-span-1' },
+    { src: 'assets/images/imagen-personas-entrenando.jpg', alt: 'Entrenamiento Libre', span: 'md:col-span-4 md:row-span-1' }
   ];
 }
